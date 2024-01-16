aa0e0d633af840c56717a517a24e49d0e49ad4bd
GSQZ <i@sayqz.com>
2024-01-16 23:40:10 +0800

    修正内容

--- public/CSMusic.js
+++ public/CSMusic.js
@@ -1,8 +1,9 @@
-{
-    "data": {
-        "Name": "Countless Simple Music",
-        "Ver": "V2.0.9",
-        "UpdateTime": "2023-10-28",
-        "DownloadUrl": "https://cdn.sayqz.com/csm/ver/V2.0.9.exe"
+// CSMusic.js
+module.exports = {
+    data: {
+        Name: "Countless Simple Music",
+        Ver: "V2.0.9",
+        UpdateTime: "2023-10-28",
+        DownloadUrl: "https://cdn.sayqz.com/csm/ver/V2.0.9.exe"
     }
-}
+};
